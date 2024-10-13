ab3b522685109154a4055971848c6348c3d8af57
ngoanhtien <ngoanhtien902@gmail.com>
2024-10-13 16:01:37 +0700

    Complete main topic course in homepage(/home)

--- frontend/main/src/components/landingpage/features/Features.tsx
+++ frontend/main/src/components/landingpage/features/Features.tsx
@@ -20,97 +20,132 @@ import {
   IconShieldLock,
   IconTag,
   IconWand,
+  IconWebhook,
 } from '@tabler/icons-react';
 import AnimationFadeIn from '../animation/Animation';
 
 interface FeaturesType {
   icon: React.ReactElement;
   title: string;
-  subtext: string;
+  // subtext: string;
 }
-
 const featuresData: FeaturesType[] = [
   {
+    icon: <IconDatabase width={40} height={40} strokeWidth={1.5} />,
+    title: 'Thiết kế và phát triển cơ sở dữ liệu',
+  },
+  {
+    icon: <IconWebhook width={40} height={40} strokeWidth={1.5} />,
+    title: 'Phát triển web',
+  },
+  {
     icon: <IconWand width={40} height={40} strokeWidth={1.5} />,
-    title: '6 Theme Colors',
-    subtext: 'We have included 6 pre-defined Theme Colors with Elegant Admin.',
+    title: 'Phát triển ứng dung di động',
   },
   {
     icon: <IconShieldLock width={40} height={40} strokeWidth={1.5} />,
-    title: 'JWT + Firebase Auth',
-    subtext: 'It is JSON Object is used to securely transfer information over the web.',
+    title: 'Ngôn ngữ lập trình',
   },
   {
     icon: <IconArchive width={40} height={40} strokeWidth={1.5} />,
-    title: '50+ Page Templates',
-    subtext: 'Yes, we have 5 demos & 50+ Pages per demo to make it easier.',
+    title: 'Mạng và bảo mật',
   },
   {
     icon: <IconAdjustments width={40} height={40} strokeWidth={1.5} />,
-    title: '45+ UI Components',
-    subtext: 'Almost 45+ UI Components being given with Modernize Admin Pack.',
+    title: 'Chứng chỉ Công nghệ thông tin',
   },
   {
     icon: <IconTag width={40} height={40} strokeWidth={1.5} />,
-    title: 'Material Ui',
-    subtext: 'Its been made with Material Ui and full responsive layout.',
+    title: 'Kiểm thử phần mềm',
   },
   {
     icon: <IconDiamond width={40} height={40} strokeWidth={1.5} />,
-    title: '3400+ Font Icons',
-    subtext: 'Lots of Icon Fonts are included here in the package of Elegant Admin.',
-  },
-  {
-    icon: <IconDatabase width={40} height={40} strokeWidth={1.5} />,
-    title: 'Axios',
-    subtext: 'Axios is a promise-based HTTP Client for node.js and the browser.',
-  },
-  {
-    icon: <IconLanguageKatakana width={40} height={40} strokeWidth={1.5} />,
-    title: 'i18 React',
-    subtext: 'react-i18 is a powerful internationalization framework for React.',
-  },
-  {
-    icon: <IconBuildingCarousel width={40} height={40} strokeWidth={1.5} />,
-    title: 'Slick Carousel',
-    subtext: 'The Last React Carousel You will Ever Need!',
-  },
-  {
-    icon: <IconArrowsShuffle width={40} height={40} strokeWidth={1.5} />,
-    title: 'Easy to Customize',
-    subtext: 'Customization will be easy as we understand your pain.',
-  },
-  {
-    icon: <IconChartPie width={40} height={40} strokeWidth={1.5} />,
-    title: 'Lots of Chart Options',
-    subtext: 'You name it and we have it, Yes lots of variations for Charts.',
-  },
-  {
-    icon: <IconLayersIntersect width={40} height={40} strokeWidth={1.5} />,
-    title: 'Lots of Table Examples',
-    subtext: 'Data Tables are initial requirement and we added them.',
-  },
-  {
-    icon: <IconRefresh width={40} height={40} strokeWidth={1.5} />,
-    title: 'Regular Updates',
-    subtext: 'We are constantly updating our pack with new features.',
-  },
-  {
-    icon: <IconBook width={40} height={40} strokeWidth={1.5} />,
-    title: 'Detailed Documentation',
-    subtext: 'We have made detailed documentation, so it will easy to use.',
-  },
-  {
-    icon: <IconCalendar width={40} height={40} strokeWidth={1.5} />,
-    title: 'Calendar Design',
-    subtext: 'Calendar is available with our package & in nice design.',
-  },
-  {
-    icon: <IconMessages width={40} height={40} strokeWidth={1.5} />,
-    title: 'Dedicated Support',
-    subtext: 'We believe in supreme support is key and we offer that.',
+    title: 'Phát triển Game',
   },
 ];
+
+// const featuresData: FeaturesType[] = [
+//   {
+//     icon: <IconWand width={40} height={40} strokeWidth={1.5} />,
+//     title: '6 Theme Colors',
+//     subtext: 'We have included 6 pre-defined Theme Colors with Elegant Admin.',
+//   },
+//   {
+//     icon: <IconShieldLock width={40} height={40} strokeWidth={1.5} />,
+//     title: 'JWT + Firebase Auth',
+//     subtext: 'It is JSON Object is used to securely transfer information over the web.',
+//   },
+//   {
+//     icon: <IconArchive width={40} height={40} strokeWidth={1.5} />,
+//     title: '50+ Page Templates',
+//     subtext: 'Yes, we have 5 demos & 50+ Pages per demo to make it easier.',
+//   },
+//   {
+//     icon: <IconAdjustments width={40} height={40} strokeWidth={1.5} />,
+//     title: '45+ UI Components',
+//     subtext: 'Almost 45+ UI Components being given with Modernize Admin Pack.',
+//   },
+//   {
+//     icon: <IconTag width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Material Ui',
+//     subtext: 'Its been made with Material Ui and full responsive layout.',
+//   },
+//   {
+//     icon: <IconDiamond width={40} height={40} strokeWidth={1.5} />,
+//     title: '3400+ Font Icons',
+//     subtext: 'Lots of Icon Fonts are included here in the package of Elegant Admin.',
+//   },
+//   {
+//     icon: <IconDatabase width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Thiết kế và phát triển cơ sở dữ liệu',
+//     subtext: 'Axios is a promise-based HTTP Client for node.js and the browser.',
+//   },
+//   {
+//     icon: <IconLanguageKatakana width={40} height={40} strokeWidth={1.5} />,
+//     title: 'i18 React',
+//     subtext: 'react-i18 is a powerful internationalization framework for React.',
+//   },
+//   {
+//     icon: <IconBuildingCarousel width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Slick Carousel',
+//     subtext: 'The Last React Carousel You will Ever Need!',
+//   },
+//   {
+//     icon: <IconArrowsShuffle width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Easy to Customize',
+//     subtext: 'Customization will be easy as we understand your pain.',
+//   },
+//   {
+//     icon: <IconChartPie width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Lots of Chart Options',
+//     subtext: 'You name it and we have it, Yes lots of variations for Charts.',
+//   },
+//   {
+//     icon: <IconLayersIntersect width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Lots of Table Examples',
+//     subtext: 'Data Tables are initial requirement and we added them.',
+//   },
+//   {
+//     icon: <IconRefresh width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Regular Updates',
+//     subtext: 'We are constantly updating our pack with new features.',
+//   },
+//   {
+//     icon: <IconBook width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Detailed Documentation',
+//     subtext: 'We have made detailed documentation, so it will easy to use.',
+//   },
+//   {
+//     icon: <IconCalendar width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Calendar Design',
+//     subtext: 'Calendar is available with our package & in nice design.',
+//   },
+//   {
+//     icon: <IconMessages width={40} height={40} strokeWidth={1.5} />,
+//     title: 'Dedicated Support',
+//     subtext: 'We believe in supreme support is key and we offer that.',
+//   },
+// ];
 
 const Features = () => {
   return (
@@ -126,9 +161,9 @@ const Features = () => {
                   <Typography variant="h5" mt={3}>
                     {feature.title}
                   </Typography>
-                  <Typography variant="subtitle1" color="textSecondary" mt={1} mb={3}>
+                  {/* <Typography variant="subtitle1" color="textSecondary" mt={1} mb={3}>
                     {feature.subtext}
-                  </Typography>
+                  </Typography> */}
                 </Grid>
               ))}
             </Grid>
